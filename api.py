19ac31a21c456f856790b3d50bc8158908316054
diao93 <162461090+diao93@users.noreply.github.com>
2025-04-05 00:40:57 +0200

    Update api.py

--- api.py
+++ api.py
@@ -25,8 +25,8 @@ cols = data_train.select_dtypes(['float64']).columns
 data_train_scaled = data_train.copy()
 data_train_scaled[cols] = StandardScaler().fit_transform(data_train[cols])
 
+# Create the SHAP explainer
 explainer = shap.TreeExplainer(model['classifier'])
-
 
 # Routes
 @app.get('/')
@@ -40,57 +40,73 @@ def check_client_id(client_id: int):
 
 @app.get('/prediction/{client_id}')
 def get_prediction(client_id: int):
-    client_data = data[data['SK_ID_CURR'] == client_id]
-    if client_data.empty:
-        return JSONResponse(content={'error': 'Client ID not found'}, status_code=404)
+    try:
+        client_data = data[data['SK_ID_CURR'] == client_id]
+        if client_data.empty:
+            return JSONResponse(content={'error': 'Client ID not found'}, status_code=404)
 
-    info_client = client_data.drop('SK_ID_CURR', axis=1)
-    prediction = model.predict_proba(info_client)[0][1]
-    return {'probability': round(float(prediction), 6)}
+        info_client = client_data.drop('SK_ID_CURR', axis=1)
+        prediction = model.predict_proba(info_client)[0][1]
+        return {'probability': round(float(prediction), 6)}
+
+    except Exception as e:
+        return JSONResponse(content={'error': str(e)}, status_code=500)
 
 @app.get('/clients_similaires/{client_id}')
 def get_data_voisins(client_id: int):
-    features = list(data_train_scaled.columns)
-    if 'SK_ID_CURR' in features: features.remove('SK_ID_CURR')
-    if 'TARGET' in features: features.remove('TARGET')
+    try:
+        features = list(data_train_scaled.columns)
+        if 'SK_ID_CURR' in features: features.remove('SK_ID_CURR')
+        if 'TARGET' in features: features.remove('TARGET')
 
-    nn = NearestNeighbors(n_neighbors=10, metric='euclidean')
-    nn.fit(data_train_scaled[features])
+        nn = NearestNeighbors(n_neighbors=10, metric='euclidean')
+        nn.fit(data_train_scaled[features])
 
-    reference_observation = data_scaled[data_scaled['SK_ID_CURR'] == client_id][features].values
-    if reference_observation.size == 0:
-        return JSONResponse(content={'error': 'Client ID not found'}, status_code=404)
+        reference_observation = data_scaled[data_scaled['SK_ID_CURR'] == client_id][features].values
+        if reference_observation.size == 0:
+            return JSONResponse(content={'error': 'Client ID not found'}, status_code=404)
 
-    indices = nn.kneighbors(reference_observation, return_distance=False)
-    df_voisins = data_train.iloc[indices[0], :]
+        indices = nn.kneighbors(reference_observation, return_distance=False)
+        df_voisins = data_train.iloc[indices[0], :]
 
-    return df_voisins.to_dict(orient='records')
+        return df_voisins.to_dict(orient='records')
+
+    except Exception as e:
+        return JSONResponse(content={'error': str(e)}, status_code=500)
 
 @app.get('/shaplocal/{client_id}')
 def shap_values_local(client_id: int):
-    client_data = data_scaled[data_scaled['SK_ID_CURR'] == client_id]
-    if client_data.empty:
-        return JSONResponse(content={'error': 'Client ID not found'}, status_code=404)
+    try:
+        client_data = data_scaled[data_scaled['SK_ID_CURR'] == client_id]
+        if client_data.empty:
+            return JSONResponse(content={'error': 'Client ID not found'}, status_code=404)
 
-    client_data = client_data.drop('SK_ID_CURR', axis=1)
-    shap_val = explainer(client_data)[0][:, 1]
+        client_data = client_data.drop('SK_ID_CURR', axis=1)
+        shap_val = explainer(client_data)[0][:, 1]
 
-    return {
-        'shap_values': shap_val.values.tolist(),
-        'base_value': shap_val.base_values,
-        'data': client_data.values.tolist(),
-        'feature_names': client_data.columns.tolist()
-    }
+        return {
+            'shap_values': shap_val.tolist(),
+            'base_value': shap_val.base_values.tolist(),
+            'data': client_data.values.tolist(),
+            'feature_names': client_data.columns.tolist()
+        }
+
+    except Exception as e:
+        return JSONResponse(content={'error': str(e)}, status_code=500)
 
 @app.get('/shap/')
 def shap_values():
-    data_no_id = data_scaled.drop('SK_ID_CURR', axis=1)
-    shap_val = explainer.shap_values(data_no_id)
+    try:
+        data_no_id = data_scaled.drop('SK_ID_CURR', axis=1)
+        shap_val = explainer.shap_values(data_no_id)
 
-    return {
-        'shap_values_0': shap_val[0].tolist(),
-        'shap_values_1': shap_val[1].tolist()
-    }
+        return {
+            'shap_values_0': shap_val[0].tolist(),
+            'shap_values_1': shap_val[1].tolist()
+        }
+
+    except Exception as e:
+        return JSONResponse(content={'error': str(e)}, status_code=500)
 
 # Run server
 if __name__ == '__main__':
