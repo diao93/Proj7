4fe114313fdb4a81958d7fcc71d953ab0bdecf28
diao93 <162461090+diao93@users.noreply.github.com>
2025-04-05 19:38:37 +0200

    Update api.py

--- api.py
+++ api.py
@@ -111,7 +111,3 @@ def shap_values():
     return {'shap_values_0': shap_val[0].tolist(),
             'shap_values_1': shap_val[1].tolist()}
 
-
-#if __name__ == '__main__':
-   # uvicorn.run(app, host='0.0.0.0', port=8000)
-    # uvicorn.run(app, host='127.0.0.1', port=8080)
